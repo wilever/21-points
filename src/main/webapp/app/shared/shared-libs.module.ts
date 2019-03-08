3c87509ff3616d058da8ef7934bb6d38ba767ffb
wilever <wilevergomez@gmail.com>
2019-03-08 17:37:20 -0300

    set us as default language

--- src/main/webapp/app/shared/shared-libs.module.ts
+++ src/main/webapp/app/shared/shared-libs.module.ts
@@ -8,7 +8,19 @@ import { CookieModule } from 'ngx-cookie';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-    imports: [NgbModule.forRoot(), InfiniteScrollModule, CookieModule.forRoot(), FontAwesomeModule],
+    imports: [
+        NgbModule.forRoot(),
+        NgJhipsterModule.forRoot({
+            // set below to true to make alerts look like toast
+            alertAsToast: false,
+            alertTimeout: 5000,
+            i18nEnabled: true,
+            defaultI18nLang: 'en'
+        }),
+        InfiniteScrollModule,
+        CookieModule.forRoot(),
+        FontAwesomeModule
+    ],
     exports: [FormsModule, CommonModule, NgbModule, NgJhipsterModule, InfiniteScrollModule, FontAwesomeModule]
 })
 export class TwentyOnePointsSharedLibsModule {
